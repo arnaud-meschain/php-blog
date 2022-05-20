bf29380e37d28b5768eb0eca5adbe1f6973da64d
Nanosss <arnaud.meschain@gmail.com>
2022-05-20 11:59:42 +0200

    mise en place du formulaire

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 Application écrite en php qui donne la possibilité aux utilisateurs de publier des articles
 Avec un system d'authentification
 ## Techno utilisées
-*PHP
+* PHP
 ## Persistance des données
-*mySQL
+* mySQL
 ## Fonctionnalités
-*Ajouter un article
-*Modifier un article
-*Supprimer un article
+* Ajouter un article
+* Modifier un article
+* Supprimer un article
--- add-article.php
+++ add-article.php
@@ -12,7 +12,46 @@
     <div class="container">
         <?php require_once'./includes/header.php' ?>
         <div class="content">
-
+            <div class="block p-20 form-container">
+                <h1>Ecrire un article</h1>
+                <form action="/add-article.php" method="POST">
+                <div class="form-control">
+                    <form action="/add-article.php" method="POST">
+                        <label for="title">Title</label>
+                        <input type="text" name="title" id="title">
+                        <p class="text-error"></p>
+                    </form>
+                </div>
+                <div class="form-control">
+                    <form action="/add-article.php" method="POST">
+                        <label for="image">Image</label>
+                        <input type="text" name="image" id="image">
+                        <p class="text-error"></p>
+                    </form>
+                </div>
+                <div class="form-control">
+                    <form action="/add-article.php" method="POST">
+                        <label for="category">Categorie</label>
+                        <select name="category" id="category">
+                            <option value="Technologie">Technologie</option>
+                            <option value="Nature">Nature</option>
+                            <option value="Politique">Politique</option>
+                        </select>
+                        <p class="text-error"></p>
+                    </form>
+                </div>
+                <div class="form-control">
+                    <form action="/add-article.php" method="POST">
+                        <label for="content">Contenu</label>
+                        <textarea name="content" id="content"></textarea>
+                        <p class="text-error"></p>
+                    </form>
+                </div>
+                <div class="form-action">
+                    <button class="btn btn-secondary">Annuler</button>
+                    <button class="btn btn-primary">Publier</button>
+                </div>
+            </div>
         </div>
         <?php require_once'./includes/footer.php' ?>
 
