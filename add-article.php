c79d67742a22e176d551d5bfcad2988657ac01d7
Nanosss <arnaud.meschain@gmail.com>
2022-05-20 12:06:16 +0200

    mise en place du formulaire corrigé

--- add-article.php
+++ add-article.php
@@ -15,22 +15,17 @@
             <div class="block p-20 form-container">
                 <h1>Ecrire un article</h1>
                 <form action="/add-article.php" method="POST">
-                <div class="form-control">
-                    <form action="/add-article.php" method="POST">
+                    <div class="form-control">
                         <label for="title">Title</label>
                         <input type="text" name="title" id="title">
                         <p class="text-error"></p>
-                    </form>
-                </div>
-                <div class="form-control">
-                    <form action="/add-article.php" method="POST">
+                    </div>
+                    <div class="form-control">
                         <label for="image">Image</label>
                         <input type="text" name="image" id="image">
                         <p class="text-error"></p>
-                    </form>
-                </div>
-                <div class="form-control">
-                    <form action="/add-article.php" method="POST">
+                    </div>
+                    <div class="form-control">
                         <label for="category">Categorie</label>
                         <select name="category" id="category">
                             <option value="Technologie">Technologie</option>
@@ -38,19 +33,17 @@
                             <option value="Politique">Politique</option>
                         </select>
                         <p class="text-error"></p>
-                    </form>
-                </div>
-                <div class="form-control">
-                    <form action="/add-article.php" method="POST">
+                    </div>
+                    <div class="form-control">
                         <label for="content">Contenu</label>
                         <textarea name="content" id="content"></textarea>
                         <p class="text-error"></p>
-                    </form>
-                </div>
-                <div class="form-action">
-                    <button class="btn btn-secondary">Annuler</button>
-                    <button class="btn btn-primary">Publier</button>
-                </div>
+                    </div>
+                    <div class="form-action">
+                        <button class="btn btn-secondary">Annuler</button>
+                        <button class="btn btn-primary">Publier</button>
+                    </div>
+                </form>
             </div>
         </div>
         <?php require_once'./includes/footer.php' ?>
